677601529b2a386d4ca20f63dc52f5ba9c856934
root <root@tienWhishper1vm8.ryo44skwzyvufbtmgla1rqrm2c.cx.internal.cloudapp.net>
2024-08-31 00:49:44 +0000

    update for FFI safe

--- cgpuvm-attest/src/lib.rs
+++ cgpuvm-attest/src/lib.rs
@@ -4,12 +4,12 @@ const MAA_ENDPOINT_URL: &str = "https://sharedeus2.eus2.attest.azure.net/";
 
 #[link(name = "azguestattestation")]
 extern {
-    fn get_attestation_token(app_data: *const u8, pcr_sel: u32, jwt: *mut u8,  jwt_len: *mut size_t, endpoint_url: *const char) -> c_int;
+    fn get_attestation_token(app_data: *const u8, pcr_sel: u32, jwt: *mut u8,  jwt_len: *mut size_t, endpoint_url: *const u8) -> c_int;
 }
 
 pub fn attest(data: &[u8], pcrs: u32) -> Option<Vec<u8>> {
     unsafe {
-        let url_ptr: *const char = MAA_ENDPOINT_URL.as_ptr() as *const char;
+        let url_ptr: *const u8 = MAA_ENDPOINT_URL.as_ptr();
         let mut dstlen = 32*1024;
         let mut dst = Vec::with_capacity(dstlen as usize);
         let pdst = dst.as_mut_ptr();
