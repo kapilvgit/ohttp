fa2f98d38d243ac93d644d4d4ec1514b256441f6
root <root@tienAccSingularityvm8.5r2ets5r043unniv24yrew0g4c.cx.internal.cloudapp.net>
2024-10-01 07:48:45 +0000

    updated

--- docker/client/run.sh
+++ docker/client/run.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [[ -z ${TARGET_PATH} ]]; then
+  echo "No TARGET defined"
+  exit 1
+fi
+
+# Generate certificate for TLS
+/usr/local/bin/ca.sh
+
 # Obtain KMS service certificate
 curl -s -k https://acceu-aml-504.confidential-ledger.azure.com/node/network | jq -r .service_certificate > service_cert.pem
 
@@ -7,6 +15,6 @@ curl -s -k https://acceu-aml-504.confidential-ledger.azure.com/node/network | jq
 curl --cacert service_cert.pem https://acceu-aml-504.confidential-ledger.azure.com/listpubkeys > keys.json
 
 # Run OHTTP client
-/usr/local/bin/ohttp-client --trust ./usr/local/bin/ca.crt \
-  'https://localhost:9443/score' -i ./examples/request.txt \
-  
+/usr/local/bin/ohttp-client --trust /usr/local/bin/ca.crt \
+  'https://100.64.3.79:9443/score' --target-path ${TARGET_PATH} -i ./examples/audio.mp3 \
+   --config `curl -s -k https://100.64.3.79:9443/discover`
