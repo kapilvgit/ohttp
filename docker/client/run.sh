ff564b1528b7bba851376eaeda06a5a70d4afae1
Kapil Vaswani <46554948+kapilvgit@users.noreply.github.com>
2024-10-04 22:43:27 +0100

    client docker changes (#14)

--- docker/client/run.sh
+++ docker/client/run.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
+if [[ -z ${TARGET} ]]; then
+  echo "No TARGET defined"
+  exit 1
+fi
+
+if [[ -z ${TARGET_PATH} ]]; then
+  echo "No TARGET_PATH defined"
+  exit 1
+fi
+
+if [[ -z ${KMS_URL} ]]; then
+  echo "No KMS_URL defined"
+  exit 1
+fi
+
+if [[ -z ${INPUT} ]]; then
+  echo "No INPUT defined"
+  exit 1
+fi
+
 # Obtain KMS service certificate
-curl -s -k https://acceu-aml-504.confidential-ledger.azure.com/node/network | jq -r .service_certificate > service_cert.pem
+curl -s -k ${KMS_URL}/node/network | jq -r .service_certificate > /tmp/service_cert.pem
 
-# Get list of public keys
-curl --cacert service_cert.pem https://acceu-aml-504.confidential-ledger.azure.com/listpubkeys > keys.json
-
-# Run OHTTP client
-/usr/local/bin/ohttp-client --trust ./usr/local/bin/ca.crt \
-  'https://localhost:9443/score' -i ./examples/request.txt \
-  
+RUST_LOG=info /usr/local/bin/ohttp-client ${TARGET}/score \
+  --target-path ${TARGET_PATH} -F "file=@${INPUT}" \
+  --kms-url ${KMS_URL} --kms-cert /tmp/service_cert.pem 
