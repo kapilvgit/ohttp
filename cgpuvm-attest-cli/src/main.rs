f2a1651cd2c005b12793ffe73d4bdf0395eeeeb5
Kapil Vaswani <kapilv@microsoft.com>
2024-10-07 09:18:26 +0000

    add maa-url

--- cgpuvm-attest-cli/src/main.rs
+++ cgpuvm-attest-cli/src/main.rs
@@ -1,7 +1,14 @@
 use cgpuvm_attest::attest;
+use std::env;
 
 pub fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        panic!("Usage: {} <maa_url>", args[0]);
+    }
+    let maa_url = &args[1];
+
     let s = "{\"a\":1}";
-    let Some(token) = attest(s.as_bytes(), 0xffff) else {panic!("Failed to get MAA token")};
+    let Some(token) = attest(s.as_bytes(), 0xffff, &maa_url) else {panic!("Failed to get MAA token")};
     println!("Got MAA token: {}", String::from_utf8(token).unwrap());
 }
